da78aafaad64890b61224a9d49837014350c1f17
Willem_Pijl <willemvanderpijl@hotmail.com>
2016-12-22 17:32:16 +0100

    TestMainActivity

--- app/src/androidTest/java/com/appje/willem/click/test/TestTok.java
+++ app/src/androidTest/java/com/appje/willem/click/test/TestTok.java
@@ -2,14 +2,28 @@ package com.appje.willem.click.test;
 
 import android.test.AndroidTestCase;
 
+import com.appje.willem.click.MainActivity;
+
+import org.junit.Test;
+
 /**
  * Created by Willem on 22/12/2016.
  */
 public class TestTok extends AndroidTestCase {
-//tok
 
-    public void TestTok() throws Throwable{
+    MainActivity TestMainActivity= new MainActivity();
 
+    @Test
+    private void TestTok() throws Throwable{
+
+        TestMainActivity.Tok();
+
+    }
+
+    @Test
+    private void TestWegTok() throws Throwable{
+
+        TestMainActivity.Tok();
     }
 
 }
